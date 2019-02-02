89420ea5a13084fb5f2ed703bd810e13dd1d3a84
IcedCoffeeBoy <lee.mingliang@outlook.com>
2019-02-02 13:49:33 +0800

    Question 1

--- index.js
+++ index.js
@@ -25,15 +25,33 @@ app.listen(3000, () => console.log('Express serevr is running at port no: 3000')
 
 
 // Get all relationship
-app.get('/relationship',(req,res)=>{
-    mysqlConnection.query('SELECT * FROM relationship', (err,rows,fields)=>{
-        if(!err){
+app.get('/relationship', (req, res) => {
+    mysqlConnection.query('SELECT * FROM relationship', (err, rows, fields) => {
+        if (!err) {
             res.send(rows)
-        }else{
+        } else {
             console.log(err)
         }
     })
 })
 
+
+// Register one or more student to a teacher
+app.post('/api/register', (req, res) => {
+    //let json = req.body;
+    let json = req.body
+    var teacher_email = json.teacher;
+    var student_array = json.students
+    var values = []
+    for (var i in student_array) {
+        values[i] = [teacher_email,student_array[i]]
+    }
+    var statement = "INSERT INTO `relationship` VALUES ?"
+    mysqlConnection.query(statement, [values], (err, rows, fields) => {
+        if (err) {
+            console.log(err)
+        }
+    })
+})
 
 
