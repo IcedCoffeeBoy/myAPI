c08cee87584b6a678662d0c978ff50accdb21eef
IcedCoffeeBoy <lee.mingliang@outlook.com>
2019-02-03 15:57:48 +0800

    deleted test script

--- trimMsg.js
+++
@@ -1,8 +0,0 @@
-function trimMessage(msg){
-    var lines = msg.split(" @")
-    return lines.slice(1)
-}
-
-var msg = "Hello students! @studentagnes@example.com @studentmiche@example.com" 
-var trimmedMsg = trimMessage(msg)
-console.log(trimmedMsg)
